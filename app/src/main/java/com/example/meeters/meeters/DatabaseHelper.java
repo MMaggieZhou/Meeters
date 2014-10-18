66ddd0579409cfededbb9f851d91cb40fce53ab2
HelloYing <zy_xu@hotmail.com>
2014-10-18 13:30:53 -0400

    I run my register page using sqlite on my computer, it works.

---
+++ app/src/main/java/com/example/meeters/meeters/DatabaseHelper.java
@@ -0,0 +1,63 @@
+package com.example.meeters.meeters;
+
+/**
+ * Created by fox on 10/18/2014.
+ */
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+public class DatabaseHelper {
+    private static final String TAG = "DBDemo_DBHelper";// 调试标签
+
+    private static final String DATABASE_NAME = "Meeters_local1.db";// 数据库名
+    SQLiteDatabase db;
+    Context context;//应用环境上下文   Activity 是其子类
+
+    DatabaseHelper(Context _context) {
+        context = _context;
+        //开启数据库
+
+        db = context.openOrCreateDatabase(DATABASE_NAME, Context.MODE_PRIVATE,null);
+        CreateTable();
+        Log.v(TAG, "db path=" + db.getPath());
+    }
+
+    public void CreateTable() {
+        try {
+            db.execSQL("CREATE TABLE t_user (" +
+                    "_ID INTEGER PRIMARY KEY autoincrement,"
+                    + "NAME TEXT,"
+                    + "email text,"
+                    + "password text"
+                    + ");");
+            Log.v(TAG, "Create Table t_user ok");
+        } catch (Exception e) {
+            Log.v(TAG, "Create Table t_user err,table exists.");
+        }
+    }
+
+    public boolean insert(String username, String email, String password){
+        String sql="";
+        try{
+            sql="insert into t_user values(null,'"+username+"', '"+email+"', '"+password+"')";
+            db.execSQL(sql);
+            Log.v(TAG,"insert Table t_user ok");
+            return true;
+
+        }catch(Exception e){
+            Log.v(TAG,"insert Table t_user err ,sql: "+sql);
+            return false;
+        }
+    }
+
+    public Cursor loadAll(){
+
+        Cursor cur=db.query("t_user", new String[]{"_ID","NAME","email","password"}, null,null, null, null, null);
+
+        return cur;
+    }
+    public void close(){
+        db.close();
+    }
+}
---
+++ app/src/main/java/com/example/meeters/meeters/RegisterActivity.java
@@ -0,0 +1,104 @@
+package com.example.meeters.meeters;
+
+/**
+ * Created by fox on 10/18/2014.
+ */
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+public class RegisterActivity extends Activity implements OnClickListener {
+    private EditText etUsername;
+    private EditText etEmail;
+    private EditText etPassword;
+    private EditText etConfirm;
+    private TextView viewAll;
+    private DatabaseHelper dh;
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        etUsername = (EditText) findViewById(R.id.username);
+        etEmail = (EditText) findViewById(R.id.email);
+        etPassword = (EditText) findViewById(R.id.password);
+        etConfirm = (EditText) findViewById(R.id.password_confirm);
+
+        View btnAdd = (Button) findViewById(R.id.done_button);
+        btnAdd.setOnClickListener(this);
+        View btnViewAll = (Button) findViewById(R.id.ViewAll_button);
+        btnViewAll.setOnClickListener(this);
+        View btnCancel = (Button) findViewById(R.id.cancel_button);
+        btnCancel.setOnClickListener(this);
+        viewAll = (TextView) findViewById(R.id.viewAll);
+
+    }
+
+    private void CreateAccount() {
+        // this.output = (TextView) this.findViewById(R.id.out_text);
+        String username = etUsername.getText().toString();
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        String confirm = etConfirm.getText().toString();
+        if ((password.equals(confirm)) && (!username.equals(""))
+                && (!password.equals("")) && (!confirm.equals(""))) {
+            this.dh = new DatabaseHelper(this);
+            this.dh.insert(username, email, password);
+            // this.labResult.setText("Added");
+            Toast.makeText(RegisterActivity.this, "new record inserted",
+                    Toast.LENGTH_SHORT).show();
+            //finish();
+        } else if ((username.equals("")) || (password.equals(""))
+                || (confirm.equals(""))) {
+            Toast.makeText(RegisterActivity.this, "Missing entry", Toast.LENGTH_SHORT)
+                    .show();
+        } else if (!password.equals(confirm)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Error")
+                    .setMessage("passwords do not match")
+                    .setNeutralButton("Try Again",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog,
+                                                    int which) {
+                                }
+                            })
+
+                    .show();
+        }
+    }
+
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.done_button:
+                CreateAccount();
+                Log.v("insert","Done");
+                //finish();
+                break;
+            case R.id.cancel_button:
+                //finish();
+                break;
+            case R.id.ViewAll_button:
+                Cursor cur = dh.loadAll();
+                StringBuffer sf = new StringBuffer();
+                cur.moveToFirst();
+                while (!cur.isAfterLast()) {
+                    sf.append(cur.getInt(0)).append(" : ").append(
+                            cur.getString(1)).append(",").append(cur.getString(3)).append("\n");
+                    cur.moveToNext();
+                }
+                //db.close();
+                viewAll.setText(sf.toString());
+                break;
+        }
+    }
+}
